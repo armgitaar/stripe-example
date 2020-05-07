f4dd05b30b4569b96f0f0bca0e8de6d2d500569b
Adam Miedema <armgitaar@yahoo.com>
2020-05-06 19:39:02 -0600

    wip

--- src/main/kotlin/routes.kt
+++ src/main/kotlin/routes.kt
@@ -1,24 +1,55 @@
 package com.stripe.example
 
-import com.stripe.example.controllers.WelcomeController
-import dev.alpas.routing.RouteGroup
+import com.stripe.Stripe
+import com.stripe.model.PaymentIntent
+import com.stripe.param.PaymentIntentCreateParams
+import dev.alpas.http.HttpCall
 import dev.alpas.routing.Router
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.gson.annotations.SerializedName
+import dev.alpas.routing.Controller
 
-// https://alpas.dev/docs/routing
 fun Router.addRoutes() = apply {
     group {
-        webRoutesGroup()
+
+        get("/", StripeController::index)
+
+        post("/create-payment-intent", StripeController::post)
+
     }.middlewareGroup("web")
-
-    apiRoutes()
 }
 
-private fun RouteGroup.webRoutesGroup() {
-    get("/", WelcomeController::index).name("welcome")
-    post("/create-payment-intent", WelcomeController::payment)
+class StripeController : Controller(){
+    fun index(call: HttpCall){
+        call.render("welcome")
+    }
 
+    fun post(call: HttpCall){
+
+        Stripe.apiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+
+        val postBody: CreatePayment = objectMapper.readValue(call.body, CreatePayment::class.java)
+        val createParams = PaymentIntentCreateParams.Builder()
+                .setCurrency("usd")
+                .setAmount(calculateOrderAmount(postBody.items).toLong())
+                .build()
+        val intent = PaymentIntent.create(createParams)
+        val paymentResponse = CreatePaymentResponse(intent.clientSecret)
+
+        call.reply(objectMapper.writeValueAsString(paymentResponse))
+    }
 }
 
-private fun Router.apiRoutes() {
-    // register API routes here
+private val objectMapper: ObjectMapper = ObjectMapper()
+
+fun calculateOrderAmount(items: Array<Any?>?): Int {
+    return 1400
 }
+
+internal class CreatePayment {
+    @SerializedName("items")
+    lateinit var items: Array<Any?>
+}
+
+internal class CreatePaymentResponse(private val clientSecret: String)
+
--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -1,5 +1,6 @@
 package com.stripe.example
 
 import dev.alpas.Alpas
+import dev.alpas.http.HttpCall
 
 fun main(args: Array<String>) = Alpas(args).routes { addRoutes() }.ignite()
